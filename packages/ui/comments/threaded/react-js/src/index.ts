a76ff6ee06316dd0bb911d4a052f35c340d4be71
Yanay Zabary <yanay.tsabary@gmail.com>
2025-10-30 19:25:51 +0200

    updae files

--- packages/ui/comments/threaded/react-js/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./components";
-export { default as useThreadedComments } from "./hooks/useThreadedComments";
-export { ThreadedStyleCallbacks } from "./components/ThreadedCommentSection";
